861727c176d44b89b5c5e91e36748d625e977279
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-11-23 15:54:51 +0000

    go.bug.st/serial basic read ok

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/timdrysdale/serial-demo
 
 go 1.18
 
-require (
-	github.com/google/gousb v1.1.2
-	github.com/sirupsen/logrus v1.9.3
-)
+require go.bug.st/serial v1.6.1
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/creack/goselect v0.1.2 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+)
--- main.go
+++ main.go
@@ -2,102 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
-	log "github.com/sirupsen/logrus"
-
-	"github.com/google/gousb"
+	"go.bug.st/serial"
 )
 
 func main() {
-	// Initialize a new Context.
-	ctx := gousb.NewContext()
 
-	ctx.Debug(4)
-
-	defer ctx.Close()
-
-	// Open any device with a given VID/PID using a convenience function.
-	dev, err := ctx.OpenDeviceWithVIDPID(0x0403, 0x6001)
-	if err != nil {
-		log.Fatalf("Could not open a device: %v", err)
+	mode := &serial.Mode{
+		BaudRate: 57600,
 	}
-	defer dev.Close()
 
-	err = dev.SetAutoDetach(true)
+	port, err := serial.Open("/dev/ttyUSB0", mode)
 	if err != nil {
-		log.Fatalf("Could not set AutoDetach true: %v", err)
+		log.Fatal(err)
 	}
-	/*
-		err = dev.Reset()
+
+	time.Sleep(1)
+
+	n, err := port.Write([]byte(`{"set":"port","to":"open"}\n\r`))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Sent %v bytes\n", n)
+
+	buff := make([]byte, 640)
+	for {
+		n, err := port.Read(buff)
 		if err != nil {
-			log.Fatalf("Could not set reset connection: %v", err)
-		}*/
-
-	// Claim the default interface using a convenience function.
-	// The default interface is always #0 alt #0 in the currently active
-	// config.
-	intf, done, err := dev.DefaultInterface()
-	if err != nil {
-		log.Fatalf("%s.DefaultInterface(): %v", dev, err)
-	}
-	defer done()
-
-	//[0x02(2,OUT) 0x81(1,IN)]
-
-	// Note handle_events: error: libusb: interrupted [code -10]
-	// https://github.com/google/gousb/issues/87
-
-	// Open an OUT endpoint.
-	ep, err := intf.OutEndpoint(2)
-	if err != nil {
-		log.Fatalf("%s.OutEndpoint(2): %v", intf, err)
-	}
-
-	// Generate some data to write.
-	data := []byte(`{"set":"port","to":"open"}\n`)
-	var numBytes int
-
-	// Write data to the USB device.
-	numBytes, err = ep.Write(data)
-	if numBytes != len(data) {
-		log.Fatalf("%s.Write([%d]): only %d bytes written, returned error is %v", ep, len(data), numBytes, err)
-	}
-	fmt.Printf("%d bytes successfully sent to the endpoint\n", len(data))
-
-	// In this interface open endpoint #6 for reading.
-	epIn, err := intf.InEndpoint(1)
-	if err != nil {
-		log.Fatalf("%s.InEndpoint(1): %v", intf, err)
-	}
-
-	go func() {
-		for i := 0; i < 100; i++ {
-			time.Sleep(1 * time.Second)
-			numBytes, err := ep.Write(data)
-			if numBytes != len(data) {
-				log.Fatalf("%s.Write([%d]): only %d bytes written, returned error is %v", ep, len(data), numBytes, err)
-			}
-			fmt.Printf("%d bytes successfully sent to the endpoint\n", len(data))
-		}
-	}()
-
-	//go func() {
-	log.Infof("%s.InEndpoint(1) MaxPacketSize: %d", intf, epIn.Desc.MaxPacketSize)
-	resp := make([]byte, epIn.Desc.MaxPacketSize*3)
-
-	for i := 0; i < 100; i++ {
-		numBytes, err = epIn.Read(resp)
-		if err != nil {
-			log.Fatalf("%s.InEndpoint(1): %v", intf, err)
+			log.Fatal(err)
 			break
 		}
-		log.Infof("read %d: %s\n", numBytes, string(resp[0:numBytes-1]))
-		time.Sleep(100 * time.Millisecond)
-		//func (e *InEndpoint) Read(buf []byte) (int, error)
+		if n == 0 {
+			fmt.Println("\nEOF")
+			break
+		}
+		fmt.Printf("%v", string(buff[:n]))
 	}
-	//}()
-
-	//time.Sleep(5)
 
 }
